fede2af65053bc46461a7fdbcac51e2db23576dd
Mateusz Oślizło <amti4osl@gmail.com>
2024-03-28 11:12:00 +0100

    make build

---
+++ index.24668ca3.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),p=getComputedStyle(e),r=p.width,l=p.height,d=getComputedStyle(t),o=d.width,s=d.height;t.style.left=parseInt(r)/2-parseInt(o)/2+"px",t.style.top=parseInt(l)/2-parseInt(s)/2+"px";
+//# sourceMappingURL=index.24668ca3.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const { width: wallWidth, height: wallHeight } = getComputedStyle(wall);
+const { width: spiderWidth, height: spiderHeight } = getComputedStyle(spider);
+spider.style.left = parseInt(wallWidth) / 2 - parseInt(spiderWidth) / 2 + "px";
+spider.style.top = parseInt(wallHeight) / 2 - parseInt(spiderHeight) / 2 + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
